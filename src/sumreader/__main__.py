082e68db78195a7cfffe19123aef1dd77e94ded3
Stojan Jovanovic <whyisepsilonneverlessthanzero@gmail.com>
2020-12-20 18:58:27 +0100

    change summary to be a monad

--- src/sumreader/__main__.py
+++ src/sumreader/__main__.py
@@ -1,22 +1,24 @@
 from functools import partial
 
-from .monad import Reader
-from .results import Summary
+from .monad import Summary
+from .results import Report
 
 import pandas as pd
 import argparse
 
 
 def main(args: argparse.Namespace) -> None:
-    def add_to_height(how_much: int, summary: "Summary"):
-        data = summary.config["data"]
+    def add_to_height(how_much: int, report: "Report"):
+        data = report.dataset
         new_data = data.assign(height=data.height + how_much)
-        return Reader(run=lambda config: Summary(config={"data": new_data}))
+        return Summary(run=lambda dataset: Report(dataset=new_data))
 
-    pipeline = Reader() >> partial(add_to_height, 20)
+    add_20_to_height = partial(add_to_height, 20)
 
-    start_config = {"data": pd.read_csv(args.path)}
-    print(pipeline(config=start_config))
+    pipeline = Summary() >> add_20_to_height
+
+    start_dataset = pd.read_csv(args.path)
+    print(pipeline(dataset=start_dataset))
 
 
 if __name__ == "__main__":
--- src/sumreader/results.py
+++ src/sumreader/results.py
@@ -2,48 +2,21 @@ import os
 import pandas as pd
 
 
-class Dataset:
-    def __init__(self):
-        raise NotImplementedError
+class Report:
+    def __init__(self, dataset: pd.DataFrame):
 
-    def __repr__(self):
-        raise NotImplementedError
-
-
-class CSVDataset(Dataset):
-    def __init__(self, path: str):
-        self._path = path
-        self._data = pd.read_csv(self._path)
-
-    def __repr__(self):
-        return str(self._data)
-
-
-class Summary:
-    def __init__(self, config: dict):
-
-        self._validate(config)
-        self.config = config
+        self.dataset = dataset
 
         self._results = set()
 
         self._summarise()
-
-    def _validate(self, config: dict):
-        try:
-            for key in {"data"}:
-                config[key]
-        except KeyError:
-            raise ValueError(f"A config must have the {key} key!")
 
     def _summarise(self):
         self._mean(column="height")
 
     def _mean(self, column: str) -> str:
         self._results.add(
-            str(
-                f"Mean of column {column} is {self.config['data'][[column]].mean()[column]}"
-            )
+            str(f"Mean of column {column} is {self.dataset[[column]].mean()[column]}")
         )
 
     def __repr__(self):
