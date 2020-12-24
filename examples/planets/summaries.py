aa698eedd371d60008920286d2d4341273ee1239
Stojan Jovanovic <whyisepsilonneverlessthanzero@gmail.com>
2020-12-24 14:26:23 +0100

    simplify report api

--- examples/planets/summaries.py
+++ examples/planets/summaries.py
@@ -3,38 +3,54 @@ import numpy as np
 
 import seaborn as sns
 from src.sumreader.monad import Summary
+from src.sumreader.data import Dataset
 from src.sumreader.results import Report
 
 
 def histogram_of_mass(bins: int, report: "Report") -> "Summary":
+    def new_run(dataset: Dataset) -> "Report":
 
-    non_na_planet_mass = list(filter(lambda v: v == v, report.dataset.planet_mass))
+        non_na_planet_mass = list(filter(lambda v: v == v, dataset.planet_mass))
 
-    counts, bin_edges = np.histogram(non_na_planet_mass, bins=bins)
+        counts, bin_edges = np.histogram(non_na_planet_mass, bins=bins)
 
-    fig, ax = plt.subplots()
-    sns.histplot(x=counts, bins=bin_edges, ax=ax)
+        fig, ax = plt.subplots()
+        sns.histplot(x=counts, bins=bin_edges, ax=ax)
 
-    ax.set_title(f"Histogram of planet mass - using {bins} bins")
+        ax.set_title(f"Histogram of planet mass - using {bins} bins")
 
-    report.results.update({f"Histogram of 'planet_mass' - {bins} bins": fig})
-    new_report_args = {"dataset": report.dataset, "results": report.results}
+        return report.add(planet_mass_hist=fig)
 
-    return Summary(run=lambda dataset: Report(**new_report_args))
+    return Summary(run=new_run)
 
 
 def boxplot_of_planet_distance(report: "Report") -> "Summary":
+    def new_run(dataset: Dataset) -> "Report":
 
-    fig, ax = plt.subplots()
-    sns.boxplot(x=report.dataset.planet_distance, ax=ax)
+        fig, ax = plt.subplots()
+        sns.boxplot(x=dataset.planet_distance, ax=ax)
 
-    ax.set_xlabel("Planet mass")
-    ax.set_title("Boxplot of planet mass")
+        ax.set_xlabel("Planet mass")
+        ax.set_title("Boxplot of planet mass")
 
-    ax.set_xscale("log")
+        ax.set_xscale("log")
 
-    report.results.update({f"Boxplot of 'planet_distnace'": fig})
+        return report.add(planet_distance_boxplot=fig)
 
-    new_report_args = {"dataset": report.dataset, "results": report.results}
+    return Summary(run=new_run)
 
-    return Summary(run=lambda dataset: Report(**new_report_args))
+
+def scatter_mass_w_distance(report: "Report") -> "Summary":
+    def new_run(dataset: Dataset) -> "Report":
+        fig, ax = plt.subplots()
+        sns.scatterplot(
+            x=np.log1p(dataset.planet_distance), y=dataset.planet_mass, ax=ax
+        )
+
+        ax.set_xlabel("Planet distance")
+        ax.set_ylabel("Planet mass")
+        ax.set_title("Scatterplot of (log) planet distance vs planet mass")
+
+        return report.add(mass_distance_scatter=fig)
+
+    return Summary(run=new_run)
---
+++ src/sumreader/data.py
@@ -0,0 +1,42 @@
+import io
+
+import requests
+import warnings
+
+import pandas as pd
+
+from enum import Enum
+
+
+class Schema(Enum):
+    pass
+
+
+class Dataset:
+    def __init__(self):
+        raise NotImplementedError
+
+
+class PandasDataset(Dataset):
+    def __init__(self, schema: "Schema"):
+
+        self._columns = (column for column in schema)
+
+        for column in self._columns:
+            setattr(self, column.name, column.value)
+
+    # when loading dataset, replace name attributes with values from real data
+    def get(self, url: str) -> "PandasDataset":
+        # read csv from url
+        data = pd.read_csv(io.StringIO(requests.get(url).content.decode("utf-8")))
+
+        # replace Dataset attributes with column values
+        for attr_name, attr_value in self.__dict__.items():
+            if attr_value in data.columns:
+                setattr(self, attr_name, data[attr_value].values)
+            else:
+                warnings.warn(
+                    f"{attr_value} (value of {attr_name} not found in columns of dataset at {url} - skipping"
+                )
+
+        return self
